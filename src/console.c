bd89c86fdf1543a93891ba551be4229358eb67e8
kevinprayoga <18221141@mahasiswa.itb.ac.id>
2022-11-03 01:18:55 +0700

    tes

---
+++ src/console.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+#include <math.h>
+#include "console.h"
+
+void start()
+{
+  
+  printf("File konfigurasi sistem berhasil dibaca. BNMO berhasil dijalankan.");
+}
---
+++ src/console.h
@@ -0,0 +1,18 @@
+#ifndef __CONSOLE_H__
+#define __CONSOLE_H__
+
+#include "boolean.h"
+#include "ADT\arraydin.h"
+#include "ADT\mesinkata.h"
+#include "ADT\queue.h"
+
+void start(char *command);
+/* I.S. : Sembarang
+   F.S. : Menampilkan pesan selamat datang dan menampilkan command yang tersedia
+   Proses : Menampilkan pesan selamat datang dan menampilkan command yang tersedia */
+
+void deleteGame();
+
+void playGame();
+
+#endif
