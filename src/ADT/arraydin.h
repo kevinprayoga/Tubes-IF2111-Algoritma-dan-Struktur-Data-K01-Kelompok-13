e4ce52d10dbcb54ca76f5dfaa1fe531961dda3d4
kevinprayoga <18221141@mahasiswa.itb.ac.id>
2022-11-02 20:05:33 +0700

    update

--- src/ADT/arraydin.h
+++ src/ADT/arraydin.h
@@ -1,13 +1,16 @@
 #ifndef __ARRAY_DINAMIK__
 #define __ARRAY_DINAMIK__
 
-#include "boolean.h"
+// Boolean
+#define boolean unsigned char
+#define true 1
+#define false 0
+
 #define InitialSize 10
 
 typedef int IdxType;
 typedef int ElType;
-typedef struct
-{
+typedef struct {
     ElType *A;
     int Capacity;
     int Neff;
@@ -58,9 +61,61 @@ int GetCapacity(ArrayDin array);
 void InsertAt(ArrayDin *array, ElType el, IdxType i);
 
 /**
+ * Fungsi untuk menambahkan elemen baru di akhir array.
+ * Prekondisi: array terdefinisi
+ */
+void InsertLast(ArrayDin *array, ElType el);
+
+/**
+ * Fungsi untuk menambahkan elemen baru di awal array.
+ * Prekondisi: array terdefinisi
+ */
+void InsertFirst(ArrayDin *array, ElType el);
+
+/**
  * Fungsi untuk menghapus elemen di index ke-i ArrayDin
  * Prekondisi: array terdefinisi, i di antara 0..Length(array).
  */
 void DeleteAt(ArrayDin *array, IdxType i);
+
+/**
+ * Fungsi untuk menghapus elemen terakhir ArrayDin
+ * Prekondisi: array tidak kosong
+ */
+void DeleteLast(ArrayDin *array);
+
+/**
+ * Fungsi untuk menghapus elemen pertama ArrayDin
+ * Prekondisi: array tidak kosong
+ */
+void DeleteFirst(ArrayDin *array);
+
+/**
+ * Fungsi untuk melakukan print suatu ArrayDin.
+ * Print dilakukan dengan format: [elemen-1, elemen-2, ..., elemen-n]
+ * dan diakhiri newline.
+ * Prekondisi: array terdefinisi
+ */
+void PrintArrayDin(ArrayDin array);
+
+/**
+ * Fungsi untuk melakukan reverse suatu ArrayDin.
+ * Prekondisi: array terdefinisi
+ */
+void ReverseArrayDin(ArrayDin *array);
+
+/**
+ * Fungsi untuk melakukan copy suatu ArrayDin.
+ * Prekondisi: array terdefinisi
+ */
+ArrayDin CopyArrayDin(ArrayDin array);
+
+/**
+ * Fungsi untuk melakukan search suatu ArrayDin.
+ * Index pertama yang ditemukan akan dikembalikan.
+ * Jika tidak ditemukan, akan mengembalikan -1.
+ * Prekondisi: array terdefinisi
+ */
+IdxType SearchArrayDin(ArrayDin array, ElType el);
 
 #endif
--- start.c
+++
@@ -1,9 +0,0 @@
-#include <stdio.h>
-#include "arraydin.h"
-#include "listgame.h"
-
-void start()
-{
-  printf("File konfigurasi sistem berhasil dibaca. BNMO berhasil dijalankan.");
-  
-}
