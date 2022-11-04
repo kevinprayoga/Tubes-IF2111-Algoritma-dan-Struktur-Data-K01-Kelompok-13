aaffc0ef15966aa68f814eda0bec1aa14c94b5ed
IbnuGifari <110520097+IbnuGifari@users.noreply.github.com>
2022-11-04 17:06:26 +0700

    Menambahkan queue game

---
+++ src/queuegame.c
@@ -0,0 +1,42 @@
+#include <stdio.h>
+#include "queue.h"
+#include "arraydin.h"
+#include "listgame.h"
+
+/*
+typedef char *ElType;
+typedef struct
+{
+  ElType buffer[CAPACITY];
+  int idxHead;
+  int idxTail;
+} Queue;
+*/
+
+void QueueGame(Queue *Game ,ArrayDin list_game){
+    int a; // integer masukan game ke-
+
+    //menampilkan daftar antrian game yang akan dimainkan
+    printf("Berikut adalah daftar antrian game-mu\n");
+    for (int i = 0; i<= (Game->idxTail); i++)
+    {
+        printf(">>%d. %s",i+1, Game->buffer[i]);
+    }
+
+    //menampilkan game yang tersedia apabila user ingin menambahkan game baru ke dalam antrian
+    printf("Berikut adalah daftar game yang tersedia\n");
+    ListGame(list_game);
+    scanf("%d", &a);
+    printf("Nomor Game yang mau ditambahkan ke antrian: %d", a);
+
+    //proses menambahkan game ke dalam antrian dari list_game
+    if (a <= Length(list_game))
+    {
+        enqueue(Game, list_game.A[a-1]);
+        printf("Game berhasil ditambahkan kedalam daftar antrian.");
+    }
+    else
+    {
+        printf("Nomor permainan tidak valid, silahkan masukkan nomor game pada list.");
+    }
+}
