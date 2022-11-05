4887bf68af05dd2b456f5ceedc9c557f6379b087
naufalmuhammadn <18221089@std.stei.itb.ac.id>
2022-11-05 11:56:07 +0700

    Memperbaiki game RNG
    mengubah scanf dengan mesinkata

--- src/gameRNG.c
+++ src/gameRNG.c
@@ -6,7 +6,8 @@ void rng() {
     int ans;
     int i = 1;
     printf("Tebakan : ");
-    scanf("%d", &ans);
+    STARTWORD();
+    ans = wordToInt(currentWord);
     while (ans != rng){
         i++;
         if (ans > rng){
@@ -15,8 +16,20 @@ void rng() {
             printf("Lebih besar.\n");
         }
         printf("Tebakan : ");
-        scanf("%d", &ans);
+        STARTWORD();
+        ans = wordToInt(currentWord);
    }
    printf("Ya, Tebakan adalah %d\n", rng);
    printf("Skor akurasi anda adalah %d/100.\n", 100/i);
+}
+
+int wordToInt(Word kata)
+{
+    int i = 0;
+    int ans = 0;
+    while (i < kata.Length){
+        ans = ans*10 + (kata.TabWord[i] - '0');
+        i++;
+    }
+    return ans;
 }
