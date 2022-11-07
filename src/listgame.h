0f34ff2429fc3225f6ff5948a63048e14db690cf
sherraaa <102856322+sherraaa@users.noreply.github.com>
2022-11-07 22:11:37 +0700

    * moment

--- src/listgame.h
+++ src/listgame.h
@@ -4,7 +4,7 @@
 #ifndef LIST_GAME_
 #define LIST_GAME_
 
-void ListGame(ArrayDin list_game);
+void ListGame(ArrayDin *list_game);
 /*
 Menampilkan daftar game yang disediakan oleh sistem
 Prekondisi: list_game tidak mungkin kosong karena 5 game pada konfigurasi
