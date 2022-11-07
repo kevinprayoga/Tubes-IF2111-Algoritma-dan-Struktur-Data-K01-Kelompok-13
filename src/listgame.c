59740fbed6010759b2df905415d818ee8679b883
sherraaa <102856322+sherraaa@users.noreply.github.com>
2022-11-07 22:07:20 +0700

    Update listgame.c
    - ganti print

--- src/listgame.c
+++ src/listgame.c
@@ -31,7 +31,7 @@ Prekondisi: list_game tidak mungkin kosong karena 5 game pada konfigurasi
             printf("Berikut adalah daftar game yang tersedia\n");
             for (i = 0; i < Length(*list_game); i++)
             {
-                printf(">>%d %s", i+1, (*list_game).A[i]);
+                printf("%d. %s\n", i, (*list_game).A[i]);
             }
         }
     }
