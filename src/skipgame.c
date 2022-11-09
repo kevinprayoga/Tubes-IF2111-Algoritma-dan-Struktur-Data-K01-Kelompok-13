c06b7000dc73b5ddcf30aa842b6069197efc6585
IbnuGifari <110520097+IbnuGifari@users.noreply.github.com>
2022-11-09 12:31:18 +0700

    revisi skipgame

--- src/skipgame.c
+++ src/skipgame.c
@@ -1,29 +1,10 @@
 #include "skipgame.h"
 
-boolean isInteger(char arg)
-{
-    boolean k;
-	if ((arg>='0') && (arg<='9')){
-        k = 1;
-    }
-    return k;
-}
-
-Queue skipQueue(Queue* Game, int a){
-    Queue New;
-    CreateQueue(&New);
-    enqueue(&New, Game->buffer[a-1]);
-    for(int i = 0; i <= IDX_TAIL(*Game)-1; i++){
-        if (Game->buffer[i] != Game->buffer[a-1]){
-            enqueue(&Game, Game->buffer[i]);
-        }
-    }
-    return New;
-}
-
 void SkipGame(Queue* Game, int command3)
 {
-    skipQueue(Game, command3);
+    char* var;
+    for(int i=0; i<command3; i++){
+        dequeue(Game, var);
+    }
     PLAYGAME(Game);
-
 }
--- src/skipgame.h
+++ src/skipgame.h
@@ -11,15 +11,6 @@
 #ifndef SKIP_GAME_
 #define SKIP_GAME_
 
-// fungsi untuk mengecek apakah argumen merupakan masukan integer atau tidak
-boolean isInteger(char arg);
-
-
-//fungsi untuk mengubah urutan queue dengan buffer ke-a menjadi urutan pertama
-Queue skipQueue(Queue* Game, int a);
-
-
-
 void SkipGame(Queue* Game, int command3);
 /*
 I.S: Queue game yang sudah memiliki urutan sesuai.
