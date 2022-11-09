310ca6de1fdd4de21b46be948059f89972ba1d30
naufalmuhammadn <18221089@std.stei.itb.ac.id>
2022-11-10 01:55:31 +0700

    Update quit.c

--- src/quit.c
+++ src/quit.c
@@ -1,20 +1,22 @@
 #include "quit.h"
+#include "save.h"
+#include "coba.h"
 
 
-void Quit(){
+void Quit(ArrayDin list){
     printf("Apakah anda ingin melakukan SAVE? (Y/N) ");
     STARTWORD();
+    char* filename;
     if (currentWord.TabWord[0] == 'Y' || currentWord.TabWord[0] == 'y'){
-        printf("melakukan save\n");
-        printf("Anda kelur dari game BNMO.\n");
-        printf("Bye bye...\n");
-        exit(0);
-    } else if (currentWord.TabWord[0] == 'N' || currentWord.TabWord[0] == 'n'){
+        printf("Masukkan nama file: \n");
+        STARTWORD();
+        filename = wordToString(currentWord);
+        save(filename, list);
         printf("Anda kelur dari game BNMO.\n");
         printf("Bye bye...\n");
         exit(0);
     } else{
         printf("Command tidak dikenali, silakan masukan command yang valid.\n");
-        Quit();
+        Quit(list);
     }
 }
