699bcb5d0982de9266e89e8de9bbaba67b8f44e5
naufalmuhammadn <18221089@std.stei.itb.ac.id>
2022-11-11 15:40:02 +0700

    Update gameRNG.c

--- src/gameRNG.c
+++ src/gameRNG.c
@@ -1,6 +1,14 @@
 #include "gameRNG.h"
 
 void rng() {
+    printf(".------..------..------.\n");
+    printf("|R.--. ||N.--. ||G.--. |\n");
+    printf("| :(): || :(): || :/\\: |\n");
+    printf("| ()() || ()() || :\\/: |\n");
+    printf("| '--'R|| '--'N|| '--'G|\n");
+    printf("`------'`------'`------'\n");
+    printf("Selamat datang di game RNG!\n");
+    printf("\nRNG Telah dimulai. Uji keberuntungan Anda dengan menebak angka.\n");
     srand(time(0));
     int rng = rand()%50;
     int ans;
