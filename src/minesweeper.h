e42d8bef70f5804962937e7ce2bc3baf2036ac85
kevinprayoga <18221141@mahasiswa.itb.ac.id>
2022-11-11 20:58:12 +0700

    Update minesweeper.h

--- src/minesweeper.h
+++ src/minesweeper.h
@@ -6,7 +6,8 @@
 #define undef -1
 #define RESET "\033[0m"
 
-typedef struct cell{
+typedef struct cell
+{
 	int x;
 	int y;
 	boolean is_mine;
@@ -15,26 +16,27 @@ typedef struct cell{
 	boolean is_flag;
 } cell;
 
-typedef struct minefield{
+typedef struct minefield
+{
 	int row;
 	int col;
-	cell* cells;
+	cell *cells;
 } minefield;
 
-minefield* create_field(int row, int col, int seed);
-void print_field(minefield* field);
+minefield *create_field(int row, int col, int seed);
+void print_field(minefield *field);
 void print_cell(cell cell);
-boolean interact(minefield* field, int x, int y, char command);
-boolean open_cell(minefield* field, int x, int y);
-void flag_cell(minefield* field, int x, int y);
-void open_zeros(minefield* field, int x, int y);
-boolean open_neighbours(minefield* field, int x, int y);
-void open_all(minefield* field);
-void open_all_mines(minefield* field);
-boolean verdict(minefield* field);
-int get_index(minefield* field, int x, int y);
-int neighbour_mines(minefield* field, int x, int y);
-int score(minefield* field);
+boolean interact(minefield *field, int x, int y, char command);
+boolean open_cell(minefield *field, int x, int y);
+void flag_cell(minefield *field, int x, int y);
+void open_zeros(minefield *field, int x, int y);
+boolean open_neighbours(minefield *field, int x, int y);
+void open_all(minefield *field);
+void open_all_mines(minefield *field);
+boolean verdict(minefield *field);
+int get_index(minefield *field, int x, int y);
+int neighbour_mines(minefield *field, int x, int y);
+int score(minefield *field);
 int playmine();
 
 #endif
