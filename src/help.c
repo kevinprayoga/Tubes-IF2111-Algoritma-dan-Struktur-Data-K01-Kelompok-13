46972326fdf07ec59aae03009028d45b1460ed13
kevinprayoga <18221141@mahasiswa.itb.ac.id>
2022-11-20 11:17:21 +0700

    Update help.c

--- src/help.c
+++ src/help.c
@@ -2,12 +2,14 @@
 
 void help()
 {
-    printf("============ HELP ============\n");
+    printf("\n============ HELP ============\n");
     printf("1. CREATE GAME - Menambahkan game baru pada daftar game\n");
     printf("2. LIST GAME - Menampilkan daftar game yang tersedia pada sistem\n");
     printf("3. DELETE GAME - Menghapus game yang dibuat secara custom oleh pengguna\n");
     printf("4. QUEUE GAME - Mendaftarkan permainan ke dalam list game\n");
     printf("5. PLAY GAME - Memainkan sebuah permainan yang tersedia pada sistem\n");
-    printf("6. SKIP GAME - Melewatkan permainan sebanyak n\n");
-    printf("7. QUIT - Keluar dari program\n");
+    printf("6. SKIP GAME <n> - Melewatkan permainan sebanyak n\n");
+    printf("7. SAVE <filename> - Menyimpan permainan ke dalam sebuah file\n");
+    printf("8. HELP - Memberikan petunjuk fungsi masing-masing fitur\n");
+    printf("9. QUIT - Keluar dari program\n");
 }
