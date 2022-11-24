b5e6a4d743c9a2e46cd0c1b426cab9f8eec586ce
kevinprayoga <18221141@mahasiswa.itb.ac.id>
2022-11-24 20:14:52 +0700

    Update stack.c

--- src/ADT/stack.c
+++ src/ADT/stack.c
@@ -25,7 +25,7 @@ boolean IsFullStack(Stack S)
 }
 
 /* ************ Menambahkan sebuah elemen ke Stack ************ */
-void Push(Stack *S, infotype X)
+void Push(Stack *S, info X)
 /* Menambahkan X sebagai elemen Stack S. */
 /* I.S. S mungkin kosong, tabel penampung elemen stack TIDAK penuh */
 /* F.S. X menjadi TOP yang baru,TOP bertambah 1 */
@@ -35,7 +35,7 @@ void Push(Stack *S, infotype X)
 }
 
 /* ************ Menghapus sebuah elemen Stack ************ */
-void Pop(Stack *S, infotype *X)
+void Pop(Stack *S, info *X)
 /* Menghapus X dari Stack S. */
 /* I.S. S  tidak mungkin kosong */
 /* F.S. X adalah nilai elemen TOP yang lama, TOP berkurang 1 */
