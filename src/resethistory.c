4d544f8a7b72d40138fce60dd3577aad89656d00
kevinprayoga <18221141@mahasiswa.itb.ac.id>
2022-11-24 22:45:26 +0700

    Create resethistory.c

---
+++ src/resethistory.c
@@ -0,0 +1,25 @@
+#include "resethistory.h"
+#include <stdio.h>
+#include "function.h"
+
+void resetHistory(Stack *s)
+{
+  printf("\nAPAKAH KAMU YAKIN INGIN MELAKUKAN RESET HISTORY? ");
+  char *command;
+  command = readQ();
+  if (strcompare(command, "YA"))
+  {
+    while (!IsEmptyStack(*s))
+    {
+      info val;
+      Pop(s, &val);
+    }
+    printf("\nHistory berhasil di-reset.\n");
+  }
+  else if (strcompare(command, "TIDAK"))
+  {
+    printf("\nHistory tidak jadi di-reset. Berikut adalah daftar Game yang telah dimainkan\n");
+    int num = countStack(*s);
+    printStack(s, num);
+  }
+}
