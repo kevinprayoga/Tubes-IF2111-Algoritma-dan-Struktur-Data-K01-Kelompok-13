2cde229c537040b12cefa19d5a9fc2fc0ac4b7a6
naufalmuhammadn <18221089@std.stei.itb.ac.id>
2022-11-28 09:45:01 +0700

    update stack toh
    ganti nama fungsi

--- src/ADT/stackTOH.c
+++ src/ADT/stackTOH.c
@@ -5,40 +5,40 @@
 /* *** Konstruktor/Kreator *** */
 
 /* I.S. sembarang; */
-/* F.S. Membuat sebuah stack S yang kosong berkapasitas MaxEl */
+/* F.S. Membuat sebuah Tower S yang kosong berkapasitas MaxEl */
 /* jadi indeksnya antara 0.. MaxEl */
-/* Ciri stack kosong : TOP bernilai Nil */
-void CreateEmptyStack(Stack *S){
+/* Ciri Tower kosong : TOP bernilai Nil */
+void CreateEmptyTower(Tower *S){
         Top(*S) = Nil;
         for (int i = 0; i < MaxEl; i++){
             (*S).T[i] = Nil;
         }
 }
 /* ************ Predikat Untuk test keadaan KOLEKSI ************ */
-boolean IsEmptyStack(Stack S){
+boolean IsEmptyTower(Tower S){
     return (Top(S) == Nil);
 }
-/* Mengirim true jika Stack kosong: lihat definisi di atas */
-boolean IsFullStack(Stack S){
+/* Mengirim true jika Tower kosong: lihat definisi di atas */
+boolean IsFullTower(Tower S){
     return (Top(S) == (MaxEl - 1));
 }
-/* Mengirim true jika tabel penampung nilai elemen stack penuh */
+/* Mengirim true jika tabel penampung nilai elemen Tower penuh */
 
-/* ************ Menambahkan sebuah elemen ke Stack ************ */
-void Push(Stack * S, infotype X){
-    if (IsEmptyStack(*S)){
+/* ************ Menambahkan sebuah elemen ke Tower ************ */
+void PushTower(Tower * S, infotype X){
+    if (IsEmptyTower(*S)){
         Top(*S) = 0;
     } else {
         Top(*S)++;
     }
     InfoTop(*S) = X;
 }
-/* Menambahkan X sebagai elemen Stack S. */
-/* I.S. S mungkin kosong, tabel penampung elemen stack TIDAK penuh */
+/* Menambahkan X sebagai elemen Tower S. */
+/* I.S. S mungkin kosong, tabel penampung elemen Tower TIDAK penuh */
 /* F.S. X menjadi TOP yang baru,TOP bertambah 1 */
 
-/* ************ Menghapus sebuah elemen Stack ************ */
-void Pop(Stack * S, infotype* X){
+/* ************ Menghapus sebuah elemen Tower ************ */
+void PopTower(Tower * S, infotype* X){
     *X = InfoTop(*S);
     InfoTop(*S) = Nil;
     if (Top(*S) == 0){
@@ -47,6 +47,6 @@ void Pop(Stack * S, infotype* X){
        Top(*S)--;
     }
 }
-/* Menghapus X dari Stack S. */
+/* Menghapus X dari Tower S. */
 /* I.S. S  tidak mungkin kosong */
 /* F.S. X adalah nilai elemen TOP yang lama, TOP berkurang 1 */
--- src/ADT/stackTOH.h
+++ src/ADT/stackTOH.h
@@ -1,24 +1,24 @@
-#ifndef stackt_H
-#define stackt_H
+#ifndef stackTOH_H
+#define stackTOH_H
 
 #include "../boolean.h"
 
 #define Nil -1
 #define MaxEl 100
-/* Nil adalah stack dengan elemen kosong . */
+/* Nil adalah Tower dengan elemen kosong . */
 
 typedef int infotype;
 typedef int address;   /* indeks tabel */
 
-/* Contoh deklarasi variabel bertype stack dengan ciri TOP : */
+/* Contoh deklarasi variabel bertype Tower dengan ciri TOP : */
 /* Versi I : dengan menyimpan tabel dan alamat top secara eksplisit*/
 typedef struct { 
   infotype T[MaxEl]; /* tabel penyimpan elemen */
   address TOP;  /* alamat TOP: elemen puncak */
-} Stack;
-/* Definisi stack S kosong : S.TOP = Nil */
-/* Elemen yang dipakai menyimpan nilai Stack T[0]..T[MaxEl-1] */
-/* Jika S adalah Stack maka akses elemen : */
+} Tower;
+/* Definisi Tower S kosong : S.TOP = Nil */
+/* Elemen yang dipakai menyimpan nilai Tower T[0]..T[MaxEl-1] */
+/* Jika S adalah Tower maka akses elemen : */
    /* S.T[(S.TOP)] untuk mengakses elemen TOP */
    /* S.TOP adalah alamat elemen TOP */
 
@@ -28,27 +28,27 @@ typedef struct {
 
 /* ************ Prototype ************ */
 /* *** Konstruktor/Kreator *** */
-void CreateEmptyStack(Stack *S);
+void CreateEmptyTower(Tower *S);
 /* I.S. sembarang; */
-/* F.S. Membuat sebuah stack S yang kosong berkapasitas MaxEl */
+/* F.S. Membuat sebuah Tower S yang kosong berkapasitas MaxEl */
 /* jadi indeksnya antara 0.. MaxEl */
-/* Ciri stack kosong : TOP bernilai Nil */
+/* Ciri Tower kosong : TOP bernilai Nil */
 
 /* ************ Predikat Untuk test keadaan KOLEKSI ************ */
-boolean IsEmptyStack(Stack S);
-/* Mengirim true jika Stack kosong: lihat definisi di atas */
-boolean IsFullStack(Stack S);
-/* Mengirim true jika tabel penampung nilai elemen stack penuh */
+boolean IsEmptyTower(Tower S);
+/* Mengirim true jika Tower kosong: lihat definisi di atas */
+boolean IsFullTower(Tower S);
+/* Mengirim true jika tabel penampung nilai elemen Tower penuh */
 
-/* ************ Menambahkan sebuah elemen ke Stack ************ */
-void Push(Stack * S, infotype X);
-/* Menambahkan X sebagai elemen Stack S. */
-/* I.S. S mungkin kosong, tabel penampung elemen stack TIDAK penuh */
+/* ************ Menambahkan sebuah elemen ke Tower ************ */
+void PushTower(Tower * S, infotype X);
+/* Menambahkan X sebagai elemen Tower S. */
+/* I.S. S mungkin kosong, tabel penampung elemen Tower TIDAK penuh */
 /* F.S. X menjadi TOP yang baru,TOP bertambah 1 */
 
-/* ************ Menghapus sebuah elemen Stack ************ */
-void Pop(Stack * S, infotype* X);
-/* Menghapus X dari Stack S. */
+/* ************ Menghapus sebuah elemen Tower ************ */
+void PopTower(Tower * S, infotype* X);
+/* Menghapus X dari Tower S. */
 /* I.S. S  tidak mungkin kosong */
 /* F.S. X adalah nilai elemen TOP yang lama, TOP berkurang 1 */
 
