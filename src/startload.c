6db66671a84283c1d4b3b06a6f79ffe61baa646e
kevinprayoga <18221141@mahasiswa.itb.ac.id>
2022-11-29 19:23:09 +0700

    Update src startload

--- src/startload.c
+++ src/startload.c
@@ -4,23 +4,13 @@
 
 void start(ArrayDin *arr)
 {
-  load(arr, "config.txt");
-}
+  char path[100] = "../data/config.txt";
 
-void load(ArrayDin *arr, char *filename)
-{
-  FILE *file;
-  char path[100] = "../data/";
-  while (*filename != '\0')
-  {
-    path[stringLength(path)] = *filename;
-    *filename++;
-  }
   STARTFILE(path);
   CopyWordFile();
   char *strnum = wordToString(currentWord);
   int num = strToInt(strnum);
-  if (num != 0)
+  if (num > 0)
   {
     ADVFILE();
     for (int i = 0; i < num; i++)
@@ -28,6 +18,79 @@ void load(ArrayDin *arr, char *filename)
       ADVWORDFILE();
       char *name = wordToString(currentWord);
       InsertLast(arr, name);
+    }
+    printf("File konfigurasi sistem berhasil dibaca. BNMO berhasil dijalankan.\n");
+  }
+}
+
+void load(ArrayDin *arr, Stack *s, ListMap *L, char *filename)
+{
+  char path[100] = "../data/";
+  while (*filename != '\0')
+  {
+    path[stringLength(path)] = *filename;
+    *filename++;
+  }
+
+  STARTFILE(path);
+  ADVWORDFILE();
+  char *strnum = wordToString(currentWord);
+  int num = strToInt(strnum);
+  if (num > 0)
+  {
+    ADVFILE();
+    for (int i = 0; i < num; i++)
+    {
+      ADVWORDFILE();
+      char *name = wordToString(currentWord);
+      InsertLast(arr, name);
+    }
+
+    ADVFILE();
+    ADVWORDFILE();
+    char *strnum2 = wordToString(currentWord);
+    int num2 = strToInt(strnum2);
+    if (num2 > 0)
+    {
+      ADVFILE();
+      Stack s1;
+      CreateEmptyStack(&s1);
+      info val;
+      for (int i = 0; i < num2; i++)
+      {
+        ADVWORDFILE();
+        char *name2 = wordToString(currentWord);
+        Push(&s1, name2);
+      }
+      for (int i = 0; i < num2; i++)
+      {
+        Pop(&s1, &val);
+        Push(s, val);
+      }
+    }
+
+    int el = 1;
+    while (!EndWord)
+    {
+      CreateEmptyMap(L); 
+      ADVFILE();
+      ADVWORDFILE();
+      char *strnum3 = wordToString(currentWord);
+      int num3 = strToInt(strnum3);
+      if (num3 > 0)
+      {
+        ADVFILE();
+        for (int i = 0; i < num3; i++)
+        {
+          ADVWORDFILEWOBLANK();
+          char *name3 = wordToString(currentWord);
+          ADVWORDFILE();
+          char *strscore = wordToString(currentWord);
+          int score = strToInt(strscore);
+          InsertMap(L, name3, score, el);
+        }
+      }
+      el++;
     }
     printf("File konfigurasi sistem berhasil dibaca. BNMO berhasil dijalankan.\n");
   }
--- src/startload.h
+++ src/startload.h
@@ -6,13 +6,15 @@
 #include "ADT\mesinkata.h"
 #include "ADT\mesinfile.h"
 #include "function.h"
+#include "ADT\stack.h"
+#include "ADT\listofmap.h"
 
 void start(ArrayDin *arr);
 /* I.S. : Sembarang
    F.S. : Menampilkan pesan selamat datang dan menampilkan command yang tersedia
    Proses : Menampilkan pesan selamat datang dan menampilkan command yang tersedia */
 
-void load(ArrayDin *arr, char *filename);
+void load(ArrayDin *arr, Stack *s, ListMap *L, char *filename);
 /* I.S. : Sembarang
    F.S. : Menampilkan pesan load berhasil dan memasukkan data dari file ke array
    Proses : Menampilkan pesan load berhasil dan memasukkan data dari file ke array */
