03f82a5c36e3a2f7b860307acfc26bd1f0039056
kevinprayoga <18221141@mahasiswa.itb.ac.id>
2022-11-29 21:38:58 +0700

    update src skipgame

--- src/skipgame.c
+++ src/skipgame.c
@@ -1,25 +1,29 @@
 #include "skipgame.h"
 #include "playgame.h"
 
-void SkipGame(Queue *Game, int command3)
+void SkipGame(Queue *Game, Stack s, ListMap l, ArrayDin arr, int command3)
 {
     printf("Game berhasil di-SKIP\n");
     ElType var;
     int len = length(*Game);
-    if (command3 > 0 && command3 <= len)
+    if (command3 > 0 && command3 < len)
     {
         for (int i = 0; i < command3; i++)
         {
             dequeue(Game, &var);
         }
-        playGame(Game);
+        playGame(Game, &s, &l, arr);
     }
-    else if (command3 > len)
+    else if (command3 >= len)
     {
         for (int i = 0; i < len; i++)
         {
             dequeue(Game, &var);
-        }   
+        }
         printf("\nTidak ada permainan lagi dalam daftar game-mu.\n");
+    }
+    else
+    {
+        printf("Masukkan angka yang valid!\n");
     }
 }
