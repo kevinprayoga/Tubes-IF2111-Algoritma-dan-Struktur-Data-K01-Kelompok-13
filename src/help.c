b933c5811ae57d6487cc4c4e07f0bca90e6129d9
kevinprayoga <18221141@mahasiswa.itb.ac.id>
2022-11-29 19:23:47 +0700

    Update help.c

--- src/help.c
+++ src/help.c
@@ -9,7 +9,11 @@ void help()
     printf("4. QUEUE GAME - Mendaftarkan permainan ke dalam list game\n");
     printf("5. PLAY GAME - Memainkan sebuah permainan yang tersedia pada sistem\n");
     printf("6. SKIP GAME <n> - Melewatkan permainan sebanyak n\n");
-    printf("7. SAVE <filename> - Menyimpan permainan ke dalam sebuah file\n");
-    printf("8. HELP - Memberikan petunjuk fungsi masing-masing fitur\n");
-    printf("9. QUIT - Keluar dari program\n");
+    printf("7. SCOREBOARD - Menampilkan scoreboard dari semua permainan\n");
+    printf("8. RESET SCOREBOARD - Menghapus data permainan yang tersimpan\n");
+    printf("9. HISTORY <n> - Menampilkan daftar permainan yang telah dimainkan dengan sejumlah n\n");
+    printf("10. RESET HISTORY - Menghapus semua permainan yang telah dimainkan\n");
+    printf("11. SAVE <filename> - Menyimpan permainan ke dalam sebuah file\n");
+    printf("12. HELP - Memberikan petunjuk fungsi masing-masing fitur\n");
+    printf("13. QUIT - Keluar dari program\n");
 }
