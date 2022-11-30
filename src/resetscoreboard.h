bb0ceff279b5cef1d4e4125f6cba23b8dbc25301
kevinprayoga <18221141@mahasiswa.itb.ac.id>
2022-11-30 12:44:54 +0700

    ADD src resetscoreboard

---
+++ src/resetscoreboard.h
@@ -0,0 +1,11 @@
+#ifndef RESETSCOREBOARD__H
+#define RESETSCOREBOARD__H
+
+#include "ADT\listofmap.h"
+#include "ADT\arraydin.h"
+#include "function.h"
+#include "listgame.h"
+
+void resetscoreboard(ListMap *L, ArrayDin arr);
+
+#endif
