d13054f4e8681ba8f2dca0f6f5eecf627d2d8f6f
kevinprayoga <18221141@mahasiswa.itb.ac.id>
2022-12-01 23:49:29 +0700

    Update hangman

---
+++ src/hangman.c
@@ -0,0 +1,189 @@
+#include "hangman.h"
+
+void opening1(){ /*belum add printf*/                                                                          
+printf("                            ^???????????????????????????????????????!.                              \n");
+printf("                           .#@@@@@@@@@@@&@@@@@@@@@@@@@@@@@@@@@@@@@@@@7                              \n");
+printf("                           .#@@7.:::::.:J#@@B7::::::::::::::::::::B@@7                              \n");
+printf("                           .#@@~     .7B@@G!                      G@@7                              \n");
+printf("                           .#@@~   .J#@@G!                        G@@7                              \n");
+printf("                           .#@@~ :Y&@@P~                          G@@7                              \n");
+printf("                           .#@@?Y&@@5^                            G@@7                              \n");
+printf("                           .#@@@@&Y:                              B@@7                              \n");
+printf("                           .#@@#J:                            ~JPB@@@&G57:                          \n");
+printf("                           .#@@!                            7B@@&G5JY5B@@&5^                        \n");
+printf("                           .#@@~                           5@@B!.      :J@@&!                       \n");
+printf("                           .#@@~                          J@@G.          !@@&:                      \n");
+printf("                           .#@@~                          G@@?            #@@!                      \n");
+printf("                           .#@@~                          J@@G.          !@@&:                      \n");
+printf("                           .#@@~                           P@@B!.      :J@@&!                       \n");
+printf("                           .#@@~                            7B@@&GYJJ5B@@&5^                        \n");
+printf("                           .#@@~                              ~JP#@@@&B57:                          \n");
+printf("                           .#@@~                                  G@@7                              \n");
+printf("                           .#@@~                                .^B@@Y:.                            \n");
+printf("                           .#@@~                          .^!JPB&@@@@@@&GY7~:                       \n");
+printf("                           .#@@~                     .~?5G&@@@&B5?#@@5JP#@@@@#PJ7:                  \n");
+printf("                           .#@@~                     7@@@#PJ!^.   G@@7   :~7YG#@@B.                 \n");
+printf("                           .#@@~                      ^^:         G@@7        .:~:                  \n");
+printf("                           .#@@~                                  G@@7                              \n");
+printf("                           .#@@~                                  G@@7                              \n");
+printf("                           .#@@~                                  G@@7                              \n");
+printf("                           .#@@~                                 ~#@@5.                             \n");
+printf("                           .#@@~                               :5@@&@@#7                            \n");
+printf("                           .#@@~                             .J&@@Y:~G@@B!                          \n");
+printf("                           .#@@~                            7#@@P^    7#@@P^                        \n");
+printf("                           .#@@~                          ~B@@B!       .J&@@5.                      \n");
+printf("                            #@@~                          ?#B?           :5#B^                      \n");
+printf("                  .~77777777&@@Y!777777!:                                                           \n");
+printf("                  ~@@@@@@@@@@@@@@@@@@@@@Y                                                           \n");
+printf("                   :^^^^^^^^^^^^^^^^^^^^.                                                           ");
+}
+void opening2(){
+/*belum add printf*/
+printf("^77~     ^77!     :77!  !7!777777:  :7!!            :~7??7!^.     :~7?J?7~.      ^7!!!.      :7!!7^   ^7!!77777~       ~7777!!!7777:    .~7?J?7!:     \n");
+printf("~@@@^    B@@@!    P@@5 .#@@@&&&&&~  ?@@&.         !G@@@##&@@!   !G&@@&#&@@&P^    5@@@@J      P@@@@J   Y@@@&&&&@5       P@&&@@@@&&&@!  ^5&@@&##@@@G7   \n");
+printf(" 5@@5   7@@&@G   ~@@&: .#@@J ....   ?@@#.        Y@@&7:...^~   ?@@@J:. .^Y@@@!   5@@B@@^    ?@&B@@?   Y@@B. ....       ....?@@&^...  ~&@@P^. .:?&@@Y  \n");
+printf(" ^&@&^ .#@G?@@7  P@@J  .#@@5!777!   ?@@#.       ~@@@!         :&@@Y       P@@B   5@@7B@B   ^&@J5@@?   Y@@#77777.           7@@#.     G@@B       ?@@@^ \n");
+printf("  Y@@5 J@@! #@# ^@@#.  .#@@&&&&&G   ?@@#.       ?@@&:         ^@@@7       J@@&.  5@@7~@@J  G@G P@@?   Y@@@&&&&&~           ?@@#.    .#@@5       ~@@@! \n");
+printf("  :&@&~#@G  7@@75@@?   .#@@J ...    ?@@#.       ~@@@!         .&@@5       G@@B   5@@7 5@&^J@&^ P@@?   Y@@B.....            ?@@#.     P@@B.      J@@@: \n");
+printf("   J@@B@@~   B@#&@B.   .#@@J:::::.  ?@@&^:::::.  5@@&?^..:^!.  7@@@5~:.:~P@@&~   5@@7 :#@#&@?  P@@?   Y@@B:::::.           ?@@#.     ^#@@G!:.:^Y@@@?  \n");
+printf("   .#@@@P    !@@@@7    .#@@@@@@@@~  ?@@@@@@@@@?   7G&@@&&@@&^   ^5#@@@&@@@#Y:    5@@7  7@@@P   P@@?   Y@@@@@@@@5           ?@@&.      :JB@@@&@@@#P~   \n");
+printf("    :~~~.     ~~~~      ^~~~~~~~~.  :~~~~~~~~~:     :~!7!!^:      .^~!7!~:.      ^~~:   ~~~.   ^~~:   :~~~~~~~~^           :~~~          :~!7!~^.     \n");
+printf("                                                                                                                                                      \n");
+printf("                                                                                                                                                      \n");
+printf("                                                                                                                                                      \n");
+printf("                                                                                                                                                      \n");
+printf("                                                                                                                                                      \n");
+printf("                                                                                                                                                      \n");
+printf("                JYY!     :YYY:      ^5555^      ^YYYY~     :YYJ      :!J5PPP5J7.   ?YYYJ.      !YYYY:      ^5555^      ^YYYY^     :YY?                \n");
+printf("               .#@@J     ^@@@~     .B@@@@B.     !@@@@&7    ^@@#.   ^P&@@#GPGB&5    B@@@@J     ^&@@@@~     .#@@@@G      !@@@@&!    ^@@#                \n");
+printf("               .B@@J     ^@@@~     5@@??@@5     ~@@GB@@J   ^@@#.  ~&@@P^           B@&G@&^    G@BB@@~     P@@7?@@Y     !@@G#@@J   ^@@B                \n");
+printf("               .B@@B55555P@@@~    7@@G  G@@7    ~@@P.G@@P. ^@@#.  P@@B   ~~~~~~.   B@&~#@G   J@@~B@@~    ?@@P  G@@!    !@@5.G@@5. ^@@B                \n");
+printf("               .B@@#GGGGGB@@@~   ^&@@7:^7@@&^   ~@@G  5@@B::&@#.  B@@P  :&@@@@@!   B@@^!@@? ~@@? #@@~   ~@@@7^:7@@&:   !@@P .5@@B::@@B                \n");
+printf("               .B@@J     ^@@@~  .B@@@@@@@@@@B.  ~@@G   ?@@&7&@#.  J@@&^  ::^&@@!   B@@^ P@&~B@G .#@@~  .B@@@@@@@@@@G   !@@P   J@@#?&@B                \n");
+printf("               .#@@J     ^@@@~  5@@B~~~~~~B@@5  !@@G    !&@@@@#.  .P@@&Y!~~7&@@!   B@@~ :&@&@&^ .#@@~  5@@B~~~~~~#@@Y  !@@G    !&@@@@B                \n");
+printf("               .G&&?     ^&&&^ !&&&~      ~&&&! ~&&P     ^B&&&B.    !5B&@@@@&#G^   G&&^  7&&&7  .B&&~ 7&&&~      !&&&~ !&&P     ^B&&&G                \n");
+printf("                ....      ...  ....        ....  ...       ....        .::::..     ...    ...    ...  ....        ....  ...      .....                \n");
+printf("                                                                                                                                                      ");
+}
+void opening3();
+void menu1();
+void menu2();
+void menu3();
+
+void penutup();
+
+
+int hangman() {
+
+	srand(time(NULL));
+	/* list kata juga seharusnya diload menggunakan load lalu masukin ke list*/
+	char ListKata[][16] = {
+		"green", 
+		"yellow",
+		"purple",
+		"windows",
+		"linux",
+		"apple",
+        "strawberry",
+        "melon",
+        "black"	
+	};
+	
+	// index for random word
+	int indeks = rand() % 9;
+	
+	int kesempatan = 10;
+	int numCorrect = 0;
+	int oldCorrect = 0;
+	char* kata = ListKata[indeks];
+	int panjangKata = stringLength(kata);
+	
+	//					     0 1 2 3 4 5
+	//				         p u r p l e
+	//  letterGuessed[8] = { 0,0,0,0,0,0,0,0 };
+	int letterGuessed[12] = { 0,0,0,0,0,0,0,0,0,0,0,0 };
+	
+	int loopIndex = 0;
+	int reguessed = 0; // EDIT
+	int skor=0;
+	
+	char guess;
+	char letterEntered;
+	
+    // game loop	
+	while ( (kesempatan != 0) ) {
+	
+		printf("\n\nNew Turn....\nKata:");
+	
+		for( loopIndex = 0; loopIndex < panjangKata; loopIndex++) {
+		
+			if(letterGuessed[loopIndex] == 1) {
+				printf("%c",kata[loopIndex]);				
+			} else {
+				printf("_");
+			}
+		
+		}	
+		
+		printf("\n");
+	
+		printf("Jumlah benar sejauh ini: %d\n",numCorrect);
+		printf("Masukkan huruf tebakan: ");
+		guess = *readQ();
+		
+		letterEntered=guess;
+		reguessed = 0; 
+		
+
+		printf("Masukan sementara: %c\n",letterEntered);
+		
+		oldCorrect = numCorrect;
+		
+		for( loopIndex = 0; loopIndex < panjangKata; loopIndex++) {
+		
+			if(letterGuessed[loopIndex] == 1) {
+				if(kata[loopIndex] == guess) {
+					reguessed = 1; 
+					break;
+				} 
+				continue;
+			}
+		
+			if(guess == kata[loopIndex] ) {
+				letterGuessed[loopIndex] = 1;
+				numCorrect++;				
+			}		
+		
+		}	
+		
+		if( oldCorrect == numCorrect && reguessed == 0) {
+			kesempatan--;
+			printf("Maaf, tebakan salah\n");
+			if (kesempatan == 0) {
+				break;
+			}
+			printf("Kesempatanmu: %d\n", kesempatan);
+		} else if( reguessed == 1) {
+			printf("Sudah ditebak sebelumnya!!\n");
+		} else {
+			printf("BENAR!! :)\n");
+		}
+		if (numCorrect == stringLength(kata)){
+			int menang = 1;
+			printf("\nKAMU MENANG!!! :)\n");
+			printf("Score kamu sejauh ini: %d\n", panjangKata);
+			/* disini maunya ada perulangan lagi supaya ga langsung end*/
+		}
+	
+	} // while loop
+	
+	if (kesempatan == 0) {
+		printf("\nMaaf, kamu kalah. Kata tebakan adalah: %s\n", kata);
+		return 0;
+	}
+}
+/*
+List yang dibutuhin:
+ngeload file txt untuk masuk ke list/array
+nambahin kata baru
+*/
