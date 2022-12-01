2b9abce17a6560e39faba08233f255ac4a821549
kevinprayoga <18221141@mahasiswa.itb.ac.id>
2022-12-01 13:02:37 +0700

    Update skipgame.c

--- src/skipgame.c
+++ src/skipgame.c
@@ -3,11 +3,11 @@
 
 void SkipGame(Queue *Game, Stack s, ListMap l, ArrayDin arr, int command3)
 {
-    printf("Game berhasil di-SKIP\n");
     ElType var;
     int len = length(*Game);
     if (command3 > 0 && command3 < len)
     {
+        printf("Game berhasil di-SKIP\n");
         for (int i = 0; i < command3; i++)
         {
             dequeue(Game, &var);
@@ -16,11 +16,19 @@ void SkipGame(Queue *Game, Stack s, ListMap l, ArrayDin arr, int command3)
     }
     else if (command3 >= len)
     {
-        for (int i = 0; i < len; i++)
+        if (!isEmpty(*Game))
         {
-            dequeue(Game, &var);
+            printf("Game berhasil di-SKIP\n");
+            for (int i = 0; i < len; i++)
+            {
+                dequeue(Game, &var);
+            }
+            printf("\nTidak ada permainan lagi dalam daftar game-mu.\n");
         }
-        printf("\nTidak ada permainan lagi dalam daftar game-mu.\n");
+        else
+        {
+            printf("Anda tidak memiliki antrian game.\n");
+        }
     }
     else
     {
